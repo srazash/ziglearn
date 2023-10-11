a8a02e90b659aa94cc87190b67ef1e54ec422094
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-10-11 23:02:11 +0100

    ownership complete

--- coding_with_zig/ownership.zig
+++ coding_with_zig/ownership.zig
@@ -6,7 +6,16 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     var lookup = std.StringHashMap(User).init(allocator);
-    defer lookup.deinit();
+    // replace the existing:
+    //   defer lookup.deinit();
+    // with:
+    defer {
+        var it = lookup.keyIterator();
+        while (it.next()) |key| {
+            allocator.free(key.*);
+        }
+        lookup.deinit();
+    }
 
     // stdin is an std.io.Reader
     // the opposite of an std.io.Writer, which we already saw
@@ -23,8 +32,17 @@ pub fn main() !void {
             if (name.len == 0) {
                 break;
             }
-            try lookup.put(name, .{ .power = i });
+            // replace the existing lookup.put with these two lines
+            const owned_name = try allocator.dupe(u8, name);
+
+            // name -> owned_name
+            try lookup.put(owned_name, .{ .power = i });
         }
+    }
+    // Place this code after the while loop
+    var it = lookup.iterator();
+    while (it.next()) |kv| {
+        std.debug.print("{s} == {any}\n", .{ kv.key_ptr.*, kv.value_ptr.* });
     }
 
     const has_leto = lookup.contains("Leto");
